82c9a2efa0455a4e3711cb5013a55ac4f00df8c4
zwehr <zwehrmeister@gmail.com>
2020-12-19 18:38:34 -0500

    make button generate random quotes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import React, { Component } from 'react';
 import './App.css';
 import QuoteBox from './QuoteBox/QuoteBox';
+import quotesData from './quotesData';
 
-function App() {
-  return (
-    <div className="App">
-      <QuoteBox />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quote: "",
+      author: ""
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateQuoteIndex();
+  }
+
+  handleClick() {
+    console.log("Click handled...");
+    this.updateQuoteIndex();
+  }
+
+  updateQuoteIndex() {
+    const newIndex = Math.floor(Math.random() * quotesData.length)
+    this.setState({
+      quote: quotesData[newIndex].quote,
+      author: quotesData[newIndex].author
+    }, () => console.log("The new quote is: " + this.state.quote + " And the new author is: " + this.state.author))
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <QuoteBox quote={this.state.quote} author={this.state.author} click={this.handleClick}/>
+      </div>
+    );
+  };
 }
 
 export default App;
--- src/QuoteBox/QuoteBox.js
+++ src/QuoteBox/QuoteBox.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const quoteBox = (props) => {
-    return (
-        <div id="quote-box">
-            <p id="text">This is the quote text.</p>
-            <p id="author">-Author</p>
-            <button id="new-quote">New Quote</button>
-        </div>
-    )
+  return (
+    <div id="quote-box">
+      <p id="text">{props.quote}</p>
+      <p id="author">{props.author}</p>
+      <button id="new-quote" onClick={props.click}>New Quote</button>
+    </div>
+  )
 }
 
 export default quoteBox;
